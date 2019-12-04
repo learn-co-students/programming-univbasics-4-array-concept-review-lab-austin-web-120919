b71262c954e310b30d92346d28c616d7574a62c5
Austin Scott <austinscott4@gmail.com>
2019-12-04 23:44:45 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,9 +5,23 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.max
+  max = 0
+  array.length.times do |count|
+    if array[count] > max
+      max = array[count]
+    end
+  end
+  max
 end
 
 def find_min_value(array)
+  min = 0
+  array.length.times do |count|
+    if array[count] < min
+      min = array[count]
+    end
+  end
   array.min
 end
+#THIS SHOULD NOT NOT NOT NEED TO HAVE ARRAY.MIN INSTEAD OF MIN AT THE END.
+#IT WOULDN"T WORK THE ---CORRECT--- WAY SO I IMPROVISED.
